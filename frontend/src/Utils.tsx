eeb24c703d963ab4436ed0bb6d6e99178d81b87c
Acsilaa-650 <gal.attila0204@gmail.com>
2025-05-22 11:36:39 +0200

    yes

--- frontend/src/Utils.tsx
+++ frontend/src/Utils.tsx
@@ -3,14 +3,19 @@ export default function f(target: string, data: object, reject: Function, accept
         let token = "";
         let hash = "";
         let base = "http://localhost:8080/"
-        fetch(base + "csrf").then((r)=>{console.log(r)})
-        // .then(r => r.json())
-        // .then((r)=>{
-        //     token = r["token"];
-        //     hash = r["hash"];
-        // }).then(()=>{
-        //     console.log(token)
-        //     console.log(token)
-        // })
+        fetch(base + 'csrf', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            credentials: 'include' // optional: only if you're dealing with cookies
+          })
+            .then(response => response.json())
+            .then(data => {
+              console.log('GET response:', data);
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
     });
 }
