acd7645cccbac9a5dfa01a0e7ba4d823d5f677c7
Acsilaa-650 <gal.attila0204@gmail.com>
2025-05-22 10:18:00 +0200

    started homepage

--- frontend/src/sites/Home/Home.tsx
+++ frontend/src/sites/Home/Home.tsx
@@ -2,7 +2,11 @@ import st from './Home.module.css';
 export const Home = () => {
     return (
     <>
-    <h1>Detective assessment</h1>
-
+    <h1 className='text-center font-semibold text-4xl sm:text-5xl mb-5'>Detective assessment</h1>
+    <p className='text-center m-0'>See below the subjects and detectives</p>
+    <h2 className='text-2xl mt-12 text-left'>Subjects</h2>
+    <div className='grid md:flex'>
+        
+    </div>
     </>)
 }
